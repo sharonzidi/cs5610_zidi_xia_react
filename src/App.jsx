91018ccec686d476a1b1f94bdcee3ed655587b88
Zidi Xia <xia.zid@northeastern.edu>
2021-10-24 01:17:11 -0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+  return (
+    <div class="grid-container">
+    <div class="grid-item1">
+        <h1 class="headerText">Hi, i'm Zidi</h1>
+    </div>
+    <div class="grid-item2"></div>
+    <div class="grid-item3">
+        <div class="square"></div>
+    </div>
+    <div class="grid-item4"></div>
+    <div class="grid-item5"></div>
+</div>
+  );
+}
+
+export default App;
